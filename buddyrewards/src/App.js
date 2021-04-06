c668a6d5e96519f6ce96780b3c21820967aba27d
poomktw <kittiwat.ktw@mail.kmutt.ac.th>
2021-04-06 15:55:52 +0700

    [ADD] create react app
    create the app by react

---
+++ buddyrewards/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>hello BuddyRewards</p>
+      <img src="./img/Aj-Like.png" />
+    </div>
+  );
+}
+
+export default App;
